8ed4dd3ae759074dab92bdc945884bc34bebac3f
IlyaSkatenok <100683690+IlyaSkatenok@users.noreply.github.com>
2022-03-12 14:37:25 +0200

    Обновлены файлы

--- Release/formula.cpp
+++ Release/formula.cpp
@@ -1,8 +1,4 @@
-#define _USE_MATH_DEFINES
 #include "Integrals.h"
-double D = 3.1415926535 / 180;
-#define Pi M_PI
-#define P pow
 double formulaFunc(double x){
-  return sqrt(x-exp(x*0.05))+tan(x*D);
+  return 1+x;
 }
